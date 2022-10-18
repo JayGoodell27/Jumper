a25239b03b4732b15be12fde7e884584a885c1ee
Jprogram01 <113635286+Jprogram01@users.noreply.github.com>
2022-10-18 13:23:41 -0600

    Word Class

---
+++ Word.cs
@@ -0,0 +1,28 @@
+
+Words WordsTest = new Words();
+string word = WordsTest.WordGive();
+WordsTest.LetterSeperate(word);
+
+
+class Words
+{
+    public string WordGive()
+    {
+        List<string> wordList = new List<string>();
+        wordList.Add("runny");
+        wordList.Add("actor");
+        wordList.Add("silly");
+        Random rnd = new Random();
+        int randIndex = rnd.Next(wordList.Count);
+        var random = wordList[randIndex];
+        return random;   
+    }
+    public List<string> LetterSeperate(WordGiven)
+    {
+
+    List<string> WordSeperated = WordGiven.ToCharArray();
+    Console.Write(WordSeperated);
+    return WordSeperated;
+    }
+
+}
