336bb9aa3fca1838f8001e084993cc581ac52772
JayGoodell27 <oreogoober@gmail.com>
2022-10-18 13:23:57 -0600

    image

--- Game.cs
+++ Game.cs
@@ -1,1 +1,17 @@
 ﻿
+
+
+
+class gameSystem
+{
+    public int stage = 1;
+    public void Main()
+    {
+        parachute chute = new parachute();
+        while (stage >= 4) {
+            Console.Write(chute.img[stage]);
+        }
+    }
+
+
+}
