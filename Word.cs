afde864086262448c3faf24b44bdff9873fe5bbc
Jprogram01 <113635286+Jprogram01@users.noreply.github.com>
2022-10-19 09:45:38 -0600

    Update word

--- Word.cs
+++ Word.cs
@@ -1,26 +1,37 @@
 
 Words WordsTest = new Words();
-string word = WordsTest.WordGive();
-WordsTest.LetterSeperate(word);
+// string word = WordsTest.WordGive();
+WordsTest.LetterSeperate();
 
 class Words
 {
-    public string WordGive()
-    {
-        List<string> wordList = new List<string>();
-        wordList.Add("runny");
-        wordList.Add("actor");
-        wordList.Add("silly");
-        Random rnd = new Random();
-        int randIndex = rnd.Next(wordList.Count);
-        var random = wordList[randIndex];
-        return random;   
-    }
+    // public string WordGive()
+    // {
+    //     List<string> wordList = new List<string>();
+    //     wordList.Add("runny");
+    //     wordList.Add("actor");
+    //     wordList.Add("silly");
+    //     Random rnd = new Random();
+    //     int randIndex = rnd.Next(wordList.Count);
+    //     var random = wordList[randIndex];
+    //     return random;   
+    // }
     public char[] LetterSeperate(string WordGive)
     {
-    string currentWord = WordGive;
+        string WordGive()
+            {
+            List<string> wordList = new List<string>();
+            wordList.Add("runny");
+            wordList.Add("actor");
+            wordList.Add("silly");
+            Random rnd = new Random();
+            int randIndex = rnd.Next(wordList.Count);
+            var random = wordList[randIndex];
+            return random;   
+            }
+    string currentWord = WordGive();
     char[] WordSeperated = currentWord.ToCharArray();
-    Console.Write(WordSeperated);
+    Console.Write(WordSeperated[1]);
     return WordSeperated;
     }
 
